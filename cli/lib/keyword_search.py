57cde964e635eb9d2e46a8bc0c23f0bbaec34b23
David M. <damckeel13@gmail.com>
2026-07-15 20:31:14 -0500

    added stop word removal from query and title search

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,12 +1,15 @@
 import string
-from .search_utils import load_movies, DEFAULT_SEARCH_LIMIT
+from tracemalloc import stop
+from .search_utils import load_movies, DEFAULT_SEARCH_LIMIT, load_stop_words
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
+    stop_words = load_stop_words()
+    pproc_stop_words = preprocess_stop_words(stop_words)
     results = []
     for m in movies:
-        pproc_query = preprocess_text(query)
-        pproc_title = preprocess_text(m["title"])
+        pproc_query = remove_stop_words(preprocess_text(query), pproc_stop_words)
+        pproc_title = remove_stop_words(preprocess_text(m["title"]), pproc_stop_words)
         found = False
         for word in pproc_query:
             for title_word in pproc_title:
@@ -25,3 +28,21 @@ def preprocess_text(text: str) -> list:
     text = text.translate(str.maketrans("","",string.punctuation))
     text_list = text.split()
     return text_list
+
+
+
+def preprocess_stop_words(list) -> list:
+    text_list = []
+    for l in list:
+        l = l.lower()
+        l = l.translate(str.maketrans("","",string.punctuation))
+        text_list.append(l)
+    return text_list
+
+
+def remove_stop_words(tokens, stop_words):
+    cleaned_words = []
+    for t in tokens:
+        if t not in stop_words:
+            cleaned_words.append(t)
+    return cleaned_words
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -4,8 +4,15 @@ import os
 DEFAULT_SEARCH_LIMIT = 5
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+STOP_WORDS = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
 
 def load_movies():
     with open(DATA_PATH, "r") as f:
         data = json.load(f)
     return data["movies"]
+
+def load_stop_words():
+    with open(STOP_WORDS, "r") as f:
+        data = f.read()
+        word_list = data.splitlines()
+    return word_list
